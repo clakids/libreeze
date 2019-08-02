5b74145af4a2d77b360e760f509eec6e1df837b9
clakids <dennis_jersey_claveria@outlook.com>
2019-08-03 01:34:06 +0800

    Added auth

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 Route::get('/login', 'PagesController@login');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
